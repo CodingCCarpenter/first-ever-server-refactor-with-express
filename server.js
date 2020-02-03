915634a2d7e5b4d71e5e391b72fad4a5dab7e048
CodingCCarpenter <christineassists@aol.com>
2020-02-03 16:34:22 -0600

    update to include server.get

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const express = require('express');
 const hostname = '127.0.0.1';
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", 'text/plain');
-    res.end('Hello World, from NodeJS');
-});
+const server = express();
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", 'text/plain');
+//     res.end('Hello World, from NodeJS');
+// });
+
+server.get('/', (req, res) => {
+    res.send('Hello World from EXPRESS!');
+})
 
 server.listen(port, () => {
     console.log(`server listening on ${port}`);
